4972e897d44c449f704e796049da4bf1d1e3cf83
rafasysop <rafaelcostamoura@gmail.com>
2020-12-15 14:31:11 -0300

    Adicionando e recuperando do localStorage

--- script.js
+++ script.js
@@ -2,46 +2,91 @@ const ulContainer = document.querySelector('#transactions');
 const totalContainer = document.querySelector('#balance');
 const receitasContainer = document.querySelector('#money-plus');
 const despesasContainer = document.querySelector('#money-minus');
+const form = document.querySelector('#form');
+const inputTransactionsName = document.querySelector('#text');
+const inputTransactionsAmount = document.querySelector('#amount');
 
 
-const dummyTransactions = [
-    { id: 1, name: 'Bolo de Brigadeiro', amaunt: -20 },
-    { id: 2, name: 'Salário', amaunt: 300 },
-    { id: 3, name: 'Torta de Frango', amaunt: -10.29 },
-    { id: 4, name: 'Violão', amaunt: 150 }
-];
+// let transactionsStorage = [
+//     { id: 1, name: 'Salário', amount: 3000 }
+// ];
+
+const localStorageTransactions = JSON.parse(localStorage
+    .getItem('transactions'));
+let transactionsStorage = localStorage
+    .getItem('transactions') !== null ? localStorageTransactions : [];
+
+
+const removeTransaction = (event) => {
+    const idTrans = +event.target.id;
+    transactionsStorage = transactionsStorage.filter(values =>
+        values.id !== idTrans);
+    console.log(transactionsStorage);
+    ulContainer.innerHTML = '';
+    init();
+    updateLocalStorage();
+}
 
 const addTransactionsIntoDOM = transaction => {
-    const operator = transaction.amaunt < 0 ? '-' : '+';
-    const CSSClass = transaction.amaunt < 0 ? 'minus' : 'plus';
-    const amauntWhithoutOperator = Math.abs(transaction.amaunt);
+    const operator = transaction.amount < 0 ? '-' : '+';
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus';
+    const amountWhithoutOperator = Math.abs(transaction.amount);
     const li = document.createElement('li');
     li.innerHTML = `
-    ${transaction.name} <span> ${operator} R$ ${amauntWhithoutOperator.toFixed(2).split('.').join(',')}</span><button class="delete-btn">x</button>
-    `
+    ${transaction.name} <span> ${operator} R$ ${amountWhithoutOperator
+    .toFixed(2).split('.').join(',')}</span>`;
+    // <button class="delete-btn">x</button>
     li.classList.add(CSSClass);
+    const button = document.createElement('button');
+    button.classList.add('delete-btn')
+    button.innerHTML = 'x';
+    button.id = transaction.id;
+    button.addEventListener('click', removeTransaction);
+    li.appendChild(button);
     ulContainer.prepend(li);
 };
 
 const updateBalanceValues = () => {
-    const total = dummyTransactions
-        .reduce((acc, value) => acc + value.amaunt, 0).toFixed(2);
-    totalContainer.textContent = `RS ${total.split('.').join(',')}`
-    const receitas = dummyTransactions
-        .filter(values => values.amaunt > 0)
-        .reduce((acc, value) => acc + value.amaunt, 0).toFixed(2);
-    receitasContainer.textContent = `RS ${receitas.split('.').join(',')}`;
-    const despesas = Math.abs(dummyTransactions
-        .filter(values => values.amaunt < 0)
-        .reduce((acc, value) => acc + value.amaunt, 0));
-    despesasContainer.textContent = ` - RS ${despesas.toFixed(2).split('.').join(',')}`;   
+    const total = transactionsStorage
+        .reduce((acc, value) => acc + value.amount, 0);
+    totalContainer.textContent = `R$ ${total.toFixed(2).split('.').join(',')}`
+    const receitas = transactionsStorage
+        .filter(values => values.amount > 0)
+        .reduce((acc, value) => acc + value.amount, 0);
+    receitasContainer.textContent = `R$ ${receitas.toFixed(2).split('.').join(',')}`;
+    const despesas = Math.abs(transactionsStorage
+        .filter(values => values.amount < 0)
+        .reduce((acc, value) => acc + value.amount, 0));
+    despesasContainer.textContent = `R$ ${despesas.toFixed(2).split('.').join(',')}`;   
 }
 
-
-
 const init = () => {
-    dummyTransactions.forEach(addTransactionsIntoDOM);
+    transactionsStorage.forEach(addTransactionsIntoDOM);
     updateBalanceValues();
 };
 
 init();
+
+const updateLocalStorage = () => {
+    localStorage.setItem('transactions', JSON.stringify(transactionsStorage));
+}
+
+const gennerateID = () => Math.round(Math.random() * 1000);
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const transactionName = inputTransactionsName.value.trim();
+    const transactionAmount = inputTransactionsAmount.value.trim();
+    if (transactionAmount === '' || transactionName === '') {
+        alert('Por favor Preencha Tanto o Nome quanto o valor da Transação');
+        return;
+    }
+    const idTransaction = gennerateID()
+    const transaction = { id: idTransaction , name: transactionName, amount: +transactionAmount };
+    transactionsStorage.push(transaction);
+    ulContainer.innerHTML = ''
+    inputTransactionsName.value = '';
+    inputTransactionsAmount.value = '';
+    init();
+    updateLocalStorage();
+});
